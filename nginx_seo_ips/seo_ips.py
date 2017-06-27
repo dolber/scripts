2faa5f0024753e76ae2cdbd6ec9f160d31f140cb
root <root@a.berkanasolutions.com>
2017-06-27 16:07:57 +0300

    scripts to generate ips from their AS number

---
+++ nginx_seo_ips/seo_ips.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import pyasn
+
+# Initialize module and load IP to ASN database
+# the sample database can be downloaded or built - see below
+# asndb = pyasn.pyasn('ipasn_20170505.0800.dat')
+asndb = pyasn.pyasn('ipasn_20170627.1200.dat')
+
+
+# print(asndb.lookup('8.8.8.8'))
+# should return: (15169, '8.8.8.0/24'), the origin AS, and the BGP prefix it matches
+
+# print(asndb.get_as_prefixes(15169))
+# print(len(asndb.get_as_prefixes(15169)))
+# returns ['130.161.0.0/16', '131.180.0.0/16', '145.94.0.0/16'], TU-Delft prefixes
+
+# corbina AS8402
+# print(asndb.get_as_prefixes(8402))
+# AS13238 - AS-YANDEX
+# AS15169 - GOOGLE
+
+# for net in asndb.get_as_prefixes(8402):
+#     print(net)
+
+print("geo $seo_user {");
+print("default 0;")
+print("");
+
+print("# AS-YANDEX networks")
+for net in asndb.get_as_prefixes(13238):
+    print(net + " 1;")
+
+print("")
+print("# GOOGLE networks")
+for net in asndb.get_as_prefixes(15169):
+    print(net + " 1;")
+
+print("")
+print("}")
+print("")
