1a2cc37535192b862e23de420e76dffff857d8e8
Alex <alex@our-dns.info>
2017-07-03 00:25:04 +0300

    add user_agent value

--- info.php
+++ info.php
@@ -3,5 +3,6 @@
 echo 'Client IP Address: '. getenv('GEOIP_ADDR')."<br>\n";
 echo 'Client Country Code: '. getenv('GEOIP_COUNTRY_CODE')."<br>\n";
 echo 'Client Country Name: '. getenv('GEOIP_COUNTRY_NAME')."<br>\n";
+echo 'Client User_Agent: '. getenv('HTTP_USER_AGENT')."<br>\n";
 
 ?>
